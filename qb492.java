7a78e92bceb6fa31a852d0408341aa332822e08e
Sachinmali8866 <157224795+Sachinmali8866@users.noreply.github.com>
2024-02-03 21:30:21 +0530

    qb492.java

---
+++ qb492.java
@@ -0,0 +1,18 @@
+//wap java program to "hello java"massage using class method
+
+import java.util.Scanner;
+
+class qb492
+
+{
+    public static void main(String[] args) {
+        qb492 s= new  qb492();
+        {
+            Scanner sc=new Scanner(System.in);
+            System.out.println("enter string");
+            String s1=sc.nextLine();
+            System.out.println(s1); 
+        }
+    }
+}
+
